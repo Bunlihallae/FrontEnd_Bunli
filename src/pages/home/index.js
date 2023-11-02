0c99c8949e4ade8a01bd5dcb7a7be1064e3cd8cd
juuuunny <102502542+juuuunny@users.noreply.github.com>
2023-11-02 20:11:37 +0900

    bunli_setting

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home=()=>{
+    return(
+        <div className="hi">
+            <div>
+                <h1 className="hello">hello</h1>
+                <p className="hello2">hello2</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
