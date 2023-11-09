78eefeba1e1a82405af561211eeaead040c53919
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-11-10 08:02:48 +0900

    로그인 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 import Giftshop from "./pages/giftshop";
 import GiftDetail from "./pages/giftDetail";
 import MyPage from "./pages/mypage";
+import Signup from "./pages/signup"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Giftshop />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/giftshop" element={<Giftshop />}></Route>
           <Route path="/giftshop/:product_id" element={<GiftDetail />}></Route>
           <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </Router>
     </>
